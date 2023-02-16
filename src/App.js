17d77f9c120839e4f92d471f89d55e16d3a87f36
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2023-02-16 08:07:25 +0700

    add Calculator APp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useState,
+  useRef
+} from "react"; 
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  const [result, setResult] = useState(0); 
+ 
+  function plus(e) { 
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputRef.current.value)); 
+  }; 
+ 
+  function minus(e) { 
+    setResult((result) => result - Number(inputRef.current.value))
+  };
+ 
+  function times(e) { 
+    setResult((result) => result * Number(inputRef.current.value))
 
-export default App;
+  }; 
+ 
+  function divide(e) { 
+    if (inputRef.current.value === "0") {
+      return;
+    }
+    setResult((result) => result / Number(inputRef.current.value))
+
+  };
+ 
+  function resetInput(e) { 
+    inputRef.current.value = ""
+  }; 
+ 
+  function resetResult(e) { 
+  	setResult(0)
+  }; 
+ 
+  return ( 
+    <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}>  {result}  </p>
+        <div>
+          <input 
+            pattern="[0-9]" 
+            ref={inputRef} 
+            type="number" 
+            placeholder="Type a number" 
+          />  
+        </div>
+        <div className="button-group">
+          <button  type="button" onClick={plus}>add</button> 
+          <button  type="button" onClick={minus}>subtract</button> 
+          <button  type="button" onClick={times}>multiply</button> 
+          <button  type="button" onClick={divide}>divide</button> 
+          <button  type="button" onClick={resetInput}>reset input</button> 
+          <button  type="button" onClick={resetResult}>reset result</button>  
+        </div> 
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
